e3f7419c4144ae418646047d9fc502ff917aadfc
Professor <muhammadprofessor77@gmail.com>
2026-02-24 04:54:57 -0800

    datatypes

---
+++ datatype.py
@@ -0,0 +1,38 @@
+price = 78
+salary = 87
+namea = "ali"
+items = ["apple" ,"book" , "chair"]
+user_info = {'name': "hamza", 'age': '45' 'address' : "multan"}
+static_items = ("u7687" , "89797" "876")
+base_values = {23 , 67, 898, 78}
+
+
+#function 
+#23,56
+#334,56787676
+
+
+def sum(val1,val2):
+    return val1 +val2
+
+value1 = 123
+value2 = 56
+sum1 = value1 + value2
+print("sum ans:", sum1 )
+#adding sum1 values by using function sum
+print("adding sum1 values by using function sum", sum(123,56))
+
+value101 = 1234
+value201 = 878
+sum2 = value101 + value201 
+print("adding sum2 values by using function,sum", sum(value101 + value201))
+
+
+
+res = sum(787686,88898)
+print(res)
+
+
+
+print(type(name))
+print(type(base_values))
