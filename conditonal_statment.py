197e70b70150b2f7c426048a7a520551c3611273
Professor <muhammadprofessor77@gmail.com>
2026-02-25 04:47:51 -0800

    conditional optaions

---
+++ conditonal_statment.py
@@ -0,0 +1,29 @@
+ #conditional statment
+ #condition operation
+ # > 
+ # <
+ # ==
+ # != not equal
+ # <=
+ # >=
+ 
+ 
+ #print(12 > 5)
+ #print(34 >= 5)
+ # value1  = 20
+ # val = 34
+ #print(value1 <=val)
+ # if single
+ #if-else
+ #if_else_i
+marks =float(input("enter your marks:"))
+ 
+ 
+if marks > 50 :
+    print("congrtulation u R pass:")
+    percentage = marks/100*100
+    print("your persontage:" ,percentage , "%")
+
+    
+    
+print("program end")    
